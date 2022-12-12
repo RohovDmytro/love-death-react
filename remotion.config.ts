cd0bc90c7b4d3ad4dfebd5b36ccbdfd31eda0e51
rohovdmytro <rogov.dmitry@gmail.com>
2022-12-12 16:12:04 +0000

    upgrade to latest; use react-native-paper

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,3 +1,44 @@
-import {Config} from 'remotion';
+import { Config } from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
+
+
+// Overwrite remotion default webpack config
+// Path: webpack.config.js
+
+
+Config.Bundling.overrideWebpackConfig((config) => {
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...(config.module?.rules ?? []),
+        {
+          test: /\.js$/,
+          exclude: /node_modules[/\\](?!react-native-vector-icons)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              // Disable reading babel configuration
+              babelrc: false,
+              configFile: false,
+
+              // The configuration for compilation
+              presets: [
+                ['@babel/preset-env'],
+                '@babel/preset-react',
+                '@babel/preset-flow',
+                "@babel/preset-typescript"
+              ],
+              plugins: [
+                '@babel/plugin-proposal-class-properties',
+                '@babel/plugin-proposal-object-rest-spread'
+              ],
+            },
+          },
+        },
+      ]
+    }
+  }
+});
